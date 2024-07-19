6c448db5e89805da5afd56dc713dbcff1b488979
tan.nguyen <raucaixanh04@gmail.com>
2024-07-19 10:25:25 +0700

    Update to customizer 0.4.0

--- customizer.php
+++ customizer.php
@@ -9,91 +9,38 @@ use AlexSkrypnyk\Customizer\CustomizeCommand;
  *
  * @SuppressWarnings(PHPMD.UnusedFormalParameter)
  */
-class CustomizerConfig {
+class Customizer {
 
   /**
-   * Messages used by the command.
+   * A required callback with question definitions.
    *
-   * @param CustomizeCommand $c
-   *   The command instance.
+   * Place questions into this method if you are using Customizer as a
+   * single-file drop-in for your scaffold project. Otherwise - place them into
+   * the configuration class.
    *
-   * @return array<string,string|array<string>>
-   *   An associative array of messages with message name as key and the message
-   *   test as a string or an array of strings.
+   * Any questions defined in the `questions()` method of the configuration
+   * class will **fully override** the questions defined here. This means that
+   * the configuration class must provide a full set of questions.
    *
-   * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-   */
-  public static function messages(CustomizeCommand $c): array {
-    return [
-      // This is an example of a custom message that overrides the default
-      // message with name `welcome`.
-      'welcome' => 'Welcome to the {{ package.name }} project customizer',
-    ];
-  }
-
-  /**
-   * Question definitions.
-   *
-   * Define questions and their processing callbacks. Questions will be asked
-   * in the order they are defined. Questions can use answers from previous
-   * questions received so far.
-   *
-   * Answers will be processed in the order they are defined. Process callbacks
-   * have access to all answers and Customizer's class public properties and
-   * methods.
-   *
-   * If a question does not have a process callback, a static method prefixed
-   * with `process` and a camel-cased question title will be called. If the
-   * method does not exist, there will be no processing.
-   *
-   * @code
-   * $questions['Machine name'] = [
-   *   // Question callback function.
-   *   'question' => fn(array $answers) => $this->io->ask(
-   *     // Question text to show to the user.
-   *     'What is your machine name',
-   *     // Default answer. Using `Str2Name` 3rd-party library to convert value.
-   *     Str2Name::machine(basename($this->cwd)),
-   *     // Answer validation function.
-   *     static fn(string $string): string => strtolower($string)
-   *   ),
-   *   // Process callback function.
-   *   'process' => function (string $title, string $answer, array $answers): void {
-   *     // Remove a directory using 'fs' and `cwd` class properties.
-   *     $this->fs->removeDirectory($this->cwd . '/somedir');
-   *     // Replace a string in a file using `cwd` class property and
-   *     // `replaceInPath` method.
-   *     $this->replaceInPath($this->cwd . '/somefile', 'old', 'new');
-   *     // Replace a string in all files in a directory.
-   *     $this->replaceInPath($this->cwd . '/somedir', 'old', 'new');
-   *   },
-   * ];
-   * @endcode
-   *
-   * @param CustomizeCommand $c
-   *   The CustomizeCommand object. Can be used to access the command properties
-   *   and methods to prepare questions. Note that the questions callbacks
-   *   already receive the command object as an argument, so this argument is
-   *   used to prepare questions array itself.
+   * See `customize.php` for an example of how to define questions.
    *
    * @return array<string,array<string,string|callable>>
    *   An associative array of questions with question title as a key and the
    *   value of array with the following keys:
-   *   - question: The question callback function used to ask the question.
+   *   - question: Required question callback function used to ask the question.
    *     The callback receives the following arguments:
+   *     - discovered: A value discovered by the discover callback or NULL.
    *     - answers: An associative array of all answers received so far.
    *     - command: The CustomizeCommand object.
-   *   - process: The callback function used to process the answer. Callback
-   *     can be an anonymous function or a method of this class as
-   *     process<PascalCasedQuestion>. The callback receives the following
+   *   - discover: Optional callback function used to discover the value from
+   *     the environment. Can be an anonymous function or a method of this class
+   *     as discover<PascalCasedQuestion>. If not provided, empty string will
+   *     be passed to the question callback. The callback receives the following
    *     arguments:
-   *     - title: The current question title.
-   *     - answer: The answer to the current question.
-   *     - answers: An associative array of all answers.
    *     - command: The CustomizeCommand object.
    *
-   * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
    * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+   * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
    */
   public static function questions(CustomizeCommand $c): array {
     // This an example of questions that can be asked to customize the project.
@@ -105,76 +52,68 @@ class CustomizerConfig {
     // You may remove all the questions below and replace them with your own.
     return [
       'Name' => [
+        // The discover callback function is used to discover the value from the
+        // environment. In this case, we use the current directory name
+        // and the GITHUB_ORG environment variable to generate the package name.
+        // @phpstan-ignore-next-line
+        'discover' => static function (CustomizeCommand $c): ?string {
+          $name = basename((string) getcwd());
+          $org = getenv('GITHUB_ORG') ?: 'acme';
+
+          return $org . '/' . $name;
+        },
         // The question callback function defines how the question is asked.
         // In this case, we ask the user to provide a package name as a string.
-        'question' => static fn(array $answers, CustomizeCommand $c): mixed => $c->io->ask('Package name', NULL, static function (string $value): string {
+        // The discovery callback is used to provide a default value.
+        // The question callback provides a capability to validate the answer
+        // before it can be accepted by providing a validation callback.
+        'question' => static fn(string $discovered, array $answers, CustomizeCommand $c): mixed => $c->io->ask('Package name', $discovered, static function (string $value): string {
           // This is a validation callback that checks if the package name is
-          // valid. If not, an exception is thrown with a message shown to the
-          // user.
+          // valid. If not, an \InvalidArgumentException exception is thrown
+          // with a message shown to the user.
           if (!preg_match('/^[a-z0-9_.-]+\/[a-z0-9_.-]+$/', $value)) {
             throw new \InvalidArgumentException(sprintf('The package name "%s" is invalid, it should be lowercase and have a vendor name, a forward slash, and a package name.', $value));
           }
 
           return $value;
         }),
-        // The process callback function defines how the answer is processed.
-        // The processing takes place only after all answers are received and
-        // the user confirms the intended changes.
-        'process' => static function (string $title, string $answer, array $answers, CustomizeCommand $c): void {
-          $name = $c->packageData['name'];
-          // Update the package data.
-          $c->packageData['name'] = $answer;
-          // Write the updated composer.json file.
-          $c->writeComposerJson($c->packageData);
-          // Replace the package name in the project files.
-          $c->replaceInPath($c->cwd, $name, $answer);
-        },
       ],
       'Description' => [
-        // For this question, we are using an answer from the previous question
+        // For this question, we use an answer from the previous question
         // in the title of the question.
-        'question' => static fn(array $answers, CustomizeCommand $c): mixed => $c->io->ask(sprintf('Description for %s', $answers['Name'])),
-        'process' => static function (string $title, string $answer, array $answers, CustomizeCommand $c): void {
-          $description = $c->packageData['description'];
-          $c->packageData['description'] = $answer;
-          $c->writeComposerJson($c->packageData);
-          $c->replaceInPath($c->cwd, $description, $answer);
-        },
+        'question' => static fn(string $discovered, array $answers, CustomizeCommand $c): mixed => $c->io->ask(sprintf('Description for %s', $answers['Name'])),
       ],
       'License' => [
-        // For this question, we are using a pre-defined list of options.
-        // For processing, we are using a separate method named 'processLicense'
+        // For this question, we use a pre-defined list of options.
+        // For discovery, we use a separate method named 'discoverLicense'
         // (only for the demonstration purposes; it could have been an
         // anonymous function).
-        'question' => static fn(array $answers, CustomizeCommand $c): mixed => $c->io->choice('License type', [
-          'MIT',
-          'GPL-3.0-or-later',
-          'Apache-2.0',
-        ], 'GPL-3.0-or-later'),
+        'question' => static fn(string $discovered, array $answers, CustomizeCommand $c): mixed => $c->io->choice('License type',
+          [
+            'MIT',
+            'GPL-3.0-or-later',
+            'Apache-2.0',
+          ],
+          // Note that the default value is the value discovered by the
+          // 'discoverLicense' method. If the discovery did not return a value,
+          // the default value of 'GPL-3.0-or-later' is used.
+          empty($discovered) ? 'GPL-3.0-or-later' : $discovered
+        ),
       ],
     ];
   }
 
   /**
-   * A callback to process the `License` question.
+   * A callback to discover the `License` value from the environment.
    *
    * This is an example callback, and it can be safely removed if this question
    * is not needed.
    *
-   * @param string $title
-   *   The question title.
-   * @param string $answer
-   *   The answer to the question.
-   * @param array<string,string> $answers
-   *   All answers received so far.
-   * @param CustomizeCommand $c
-   *   The command instance.
-   *
-   * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+   * @param \AlexSkrypnyk\Customizer\CustomizeCommand $c
+   *   The Customizer instance.
    */
-  public static function processLicense(string $title, string $answer, array $answers, CustomizeCommand $c): void {
-    $c->packageData['license'] = $answer;
-    $c->writeComposerJson($c->packageData);
+  public static function discoverLicense(CustomizeCommand $c): string {
+    return isset($c->composerjsonData['license']) && is_string($c->composerjsonData['license']) ? $c->composerjsonData['license'] : '';
   }
 
   /**
@@ -198,6 +137,26 @@ class CustomizerConfig {
     CustomizeCommand::arrayUnsetDeep($composerjson, ['autoload-dev', 'psr-4', 'AlexSkrypnyk\\TemplateProjectExample\\Tests\\']);
 
     $c->fs->remove($c->cwd . '/tests');
+  }
+
+  /**
+   * Override some of the messages displayed to the user by Customizer.
+   *
+   * @param \AlexSkrypnyk\Customizer\CustomizeCommand $c
+   *   The Customizer instance.
+   *
+   * @return array<string,string|array<string>>
+   *   An associative array of messages with message name as key and the message
+   *   test as a string or an array of strings.
+   *
+   * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+   */
+  public static function messages(CustomizeCommand $c): array {
+    return [
+      // This is an example of a custom message that overrides the default
+      // message with name `welcome`.
+      'title' => 'Welcome to the "{{ package.name }}" project customizer',
+    ];
   }
 
 }
